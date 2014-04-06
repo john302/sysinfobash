ef33347435291d8cb02fa9dec92a14247a0d07d5
john302 <johncartwright302@gmail.com>
2014-04-07 09:50:48 +1000

    Update sysinfo.sh
    Code cleanup.

--- sysinfo.sh
+++ sysinfo.sh
@@ -13,4 +13,6 @@ echo "The load average is: $(cut -f1 -d ' ' /proc/loadavg). The CPU has\
  $(grep -c ^processor /proc/cpuinfo 2>/dev/null) cores."
 echo "Current CPU usage is: $(top -bn 2 -d 0.01 | grep '^%Cpu' | 
 tail -n 1 | awk '{print $2+$4+$6}')%"
-echo "There are $(ls /var/cache/apt/archives/ | wc -l) packages in the /var/cache/apt/archives/ directory."
+echo "There are $(ls /var/cache/apt/archives/ | wc -l) packages in the\
+/var/cache/apt/archives/ directory."
+
